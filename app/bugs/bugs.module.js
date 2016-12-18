b789d57f8492fe0e15db6dd2c6ce78135e6d5c86
clickglue <clickglue@telenet.be>
2016-12-18 18:14:37 +0100

    progressing through the course

--- app/bugs/bugs.module.js
+++ app/bugs/bugs.module.js
@@ -9,18 +9,19 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
-var AppComponent = (function () {
-    function AppComponent() {
+var BugModule = (function () {
+    function BugModule() {
     }
-    AppComponent = __decorate([
-        core_1.Component({
-            selector: 'my-app',
-            template: "\n    <navbar></navbar>\n    <div class=\"container\">\n        <router-outlet></router-outlet>\n    </div>\n    ",
-            styles: ["\n        .container{\n            margin-top: 5rem;\n        }\n    "]
+    BugModule = __decorate([
+        core_1.NgModule({
+            imports: [],
+            declarations: [],
+            exports: [],
+            providers: []
         }), 
         __metadata('design:paramtypes', [])
-    ], AppComponent);
-    return AppComponent;
+    ], BugModule);
+    return BugModule;
 }());
-exports.AppComponent = AppComponent;
-//# sourceMappingURL=app.component.js.map
+exports.BugModule = BugModule;
+//# sourceMappingURL=bugs.module.js.map
